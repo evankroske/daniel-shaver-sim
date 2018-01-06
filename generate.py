3962ad0bd716ab10b5629c99109ab8ed676b0090
Evan Kroske <evan@navahq.com>
2018-01-05 23:08:09 -0500

    Removes another global array

--- generate.py
+++ generate.py
@@ -11,11 +11,27 @@ stateWithName = dict(
             'Press any button',
             'to play'
         ],
+        state_after_input=[
+            "START",
+            "START",
+            "START",
+            "START",
+            "START",
+            "START",
+        ],
     ),
     MISTAKE=dict(
         msg=[
             'Officer Langley:',
             "Don't!"
+        ],
+        state_after_input=[
+            "BEING_SHOT",
+            "BEING_SHOT",
+            "BEING_SHOT",
+            "BEING_SHOT",
+            "BEING_SHOT",
+            "BEING_SHOT",
         ],
     ),
     BEING_SHOT=dict(
@@ -42,10 +58,26 @@ stateWithName = dict(
             'Press the A Button!',
             'Press the A Button!'
         ],
+        state_after_input=[
+            "WAIT",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+        ],
     ),
     WAIT=dict(
         msg=[
             ''
+        ],
+        state_after_input=[
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
         ],
     ),
     WHO_ELSE=dict(
@@ -53,16 +85,40 @@ stateWithName = dict(
             'The first letter of',
             'the alphabet is B?'
         ],
+        state_after_input=[
+            "NOBODY_ELSE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+        ],
     ),
     NOBODY_ELSE=dict(
         msg=[
             'The first letter of',
             'the alphabet is A?'
         ],
+        state_after_input=[
+            "POSITIVE",
+            "POSITIVE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+        ],
     ),
     POSITIVE=dict(
         msg=[
             'Are you positive?'
+        ],
+        state_after_input=[
+            "FTC",
+            "FTC",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
+            "MISTAKE",
         ],
     ),
     FTC=dict(
@@ -72,6 +128,14 @@ stateWithName = dict(
             'you to comprehend',
             'simple instructions.'
         ],
+        state_after_input=[
+            "AGAIN",
+            "AGAIN",
+            "AGAIN",
+            "AGAIN",
+            "AGAIN",
+            "AGAIN",
+        ],
     ),
     AGAIN=dict(
         msg=[
@@ -80,6 +144,14 @@ stateWithName = dict(
             'OK, can you both',
             'hear and understand',
             'me?'
+        ],
+        state_after_input=[
+            "THREAT",
+            "THREAT",
+            "THREAT",
+            "THREAT",
+            "THREAT",
+            "THREAT",
         ],
     ),
     THREAT=dict(
@@ -91,6 +163,14 @@ stateWithName = dict(
             'both going to get',
             'shot. Do you',
             'understand that?'
+        ],
+        state_after_input=[
+            "QUESTION",
+            "QUESTION",
+            "QUESTION",
+            "QUESTION",
+            "QUESTION",
+            "QUESTION",
         ],
     ),
     QUESTION=dict(
@@ -119,17 +199,28 @@ $names_states
 void printMsgOfState(State state) {
   char *msg = {};
   switch (state) {
-$cases
+$casesPrintMsgOfState
   }
   arduboy.print(msg);
   delete msg;
 }
+
+State stateAfterInput(State state, int inputIndex) {
+  switch (state) {
+$casesStateAfterInput
+  default:
+    return 255;
+  }
+}
 """.strip("\n"))
     return template.substitute(
-        cases="\n".join(caseFromState(n, s) for n, s in namesAndStates),
         names_states="\n".join(
             "const State {} = {};".format(n, i)
-                for i, (n, s) in enumerate(namesAndStates)))
+                for i, (n, s) in enumerate(namesAndStates)),
+        casesPrintMsgOfState="\n".join(
+            caseFromState(n, s) for n, s in namesAndStates),
+        casesStateAfterInput="\n".join(
+            caseStateAfterInputOuter(n, s) for n, s in namesAndStates))
 
 def caseFromState(name, state):
     msg = "\n".join(state["msg"]) + "\0"
@@ -141,6 +232,27 @@ def caseFromState(name, state):
         name=name,
         len_msg=len(msg),
         msg=", ".join(r"'\x{:x}'".format(ord(c)) for c in msg))
+
+def caseStateAfterInputOuter(name, state):
+    if "state_after_input" not in state:
+        return ""
+
+    return string.Template("""
+  case $name:
+    switch (inputIndex) {
+$cases
+    default:
+      return 255;
+    }
+""".strip("\n")).substitute(
+        name=name,
+        cases="\n".join(caseStateAfterInputInner(state, i) for i in range(6)))
+
+def caseStateAfterInputInner(state, i):
+    return string.Template("""
+    case $i:
+      return $stateAfterInput;
+""".strip("\n")).substitute(i=i, stateAfterInput=state["state_after_input"][i])
 
 if __name__ == "__main__":
     main()
